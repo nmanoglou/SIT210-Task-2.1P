db92c05273b4f7483419ca5909379312819b88ed
nmanoglou <62323315+nmanoglou@users.noreply.github.com>
2020-03-18 16:43:39 +1100

    Add files via upload

---
+++ task-2-1p.ino
@@ -0,0 +1,214 @@
+
+int led1 = D6; 
+int led2 = D7;
+
+void setup() {
+
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+}
+
+void loop() {
+
+    // N
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay (1000); // long blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    // I
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    // C
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay (1000); // long blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay (1000); // long blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    
+    
+    // H
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    // O
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay (1000); // long blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay (1000); // long blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay (1000); // long blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    // L
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay (1000); // long blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    // A
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay (1000); // long blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    // S
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(500); // short blink
+    
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    delay (500); // pause between blinks
+}
